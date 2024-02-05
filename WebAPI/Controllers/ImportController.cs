b1347f6a88abfd757d4a2160bcbc433c121e3f3e
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-02-05 15:40:37 +0100

    create methods to import files

--- WebAPI/Controllers/ImportController.cs
+++ WebAPI/Controllers/ImportController.cs
@@ -22,22 +22,80 @@ namespace WebAPI.Controllers
         {
             try
             {
-                var path = "C:/Users/bigie/Downloads/Products.csv";
-                SaveFileLocally(path, "Products.csv");
+                var products_path = "C:/Users/bigie/Downloads/Products.csv";
 
-                var products = ReadProducts(path);
+                SaveFileLocally(products_path, "Products.csv");
 
-                var filtered = products.Where(n => !n.category.Contains("kable"));
-                
+                var products = ReadProducts(products_path);
+
+                var filteredProducts = products.Where(p => !p.category.Contains("kable")).ToList();
+                //gets the products that are not cables
+
+                context.Products.AddRange(filteredProducts);
+
+                context.SaveChanges();
+
             }
             catch (Exception ex)
             {
-
+                return BadRequest(ex.Message);
             }
-            return View();
+            return Ok("Successfull Import");
+        }
+
+        [HttpPost("importInventory")]
+        public IActionResult ImportInventory()
+        {
+            try
+            {
+                var inventory_path = "C:/Users/bigie/Downloads/Inventory.csv";
+                
+                SaveFileLocally(inventory_path, "Inventory.csv");
+                
+                var inventory = ReadInventory(inventory_path);
+                
+                var filteredInventory = inventory.Where(i => i.shipping_time.Contains("24")).ToList();
+                //gets only the inventory fields with shipping time up to 24 hours
+                
+                context.Inventory.AddRange(filteredInventory);
+                
+                context.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return Ok("Successfull Import");
         }
 
 
+        [HttpPost("importPrices")]
+        public IActionResult ImportPrices()
+        {
+            try
+            {
+                var price_path = "C:/Users/bigie/Downloads/Prices.csv";
+                
+                SaveFileLocally(price_path, "Prices.csv");
+                
+                var price = ReadPrice(price_path);
+                
+                context.Prices.AddRange(price);
+
+                context.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return Ok("Successfull Import");
+        }
+
+        /// <summary>
+        /// Converts the file from csv file path to the List of products
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
         private List<Product> ReadProducts(string path)
         {
             var products = new List<Product>();
@@ -51,10 +109,45 @@ namespace WebAPI.Controllers
             return products;
         }
 
+        /// <summary>
+        /// Converts the file from csv file path to the List of Inventories
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private List<Inventory> ReadInventory(string path)
+        {
+            var inventory = new List<Inventory>();
+
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                inventory = csv.GetRecords<Inventory>().ToList();
+            }
+
+            return inventory;
+        }
+        /// <summary>
+        /// Converts the file from csv file path to the List of Prices
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private List<Price> ReadPrice(string path)
+        {
+            var price = new List<Price>();
+
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                price = csv.GetRecords<Price>().ToList();
+            }
+
+            return price;
+        }
+
         private void SaveFileLocally(string sourceFilePath, string destinationFileName)
         {
             var destinationFilePath = $"C:/{destinationFileName}";
-            System.IO.File.Copy(sourceFilePath, destinationFilePath, true);
+            System.IO.File.Copy(sourceFilePath, destinationFilePath, true); //copies the file from its source path to the destination provided
         }
     }
 }
